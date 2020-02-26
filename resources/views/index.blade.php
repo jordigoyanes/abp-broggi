9a6af5a1f371597097e69253a5ffff070b0e3043
david345672 <davidmur12@gmail.com>
2020-02-26 13:14:58 +0100

    create master2 template navbar2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('templates.master')
+@extends('templates.master2')
 
 @section('titulo')
     HOME
---
+++ resources/views/templates/master2.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('titulo')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> 
+    
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+</head>
+
+
+
+<body>
+        
+      <nav class="navbar navbar-expand-lg navbar-light bg-transaparent border-bottom">
+        <a class="navbar-brand" href="#"><img src="./img/logo.png" class="ml-5" style="width:60px;height:60px;"  alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+                        
+          </ul>          
+        </div>
+      </nav>
+
+
+
+
+</body>
+
+<div class="container">
+    @yield('principal')
+</div>
+
+</html>
