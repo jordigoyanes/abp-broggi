7545a18b8aa175d20014b920e9badf0efb0f14a5
david345672 <davidmur12@gmail.com>
2020-03-03 11:42:43 +0100

    change bootstrap masters

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> 
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
