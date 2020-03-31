aa71001f0d47751aa2cc383250bd532b6a1d4a54
Jordi Goyanes Granados <jordigoyanes@gmail.com>
2020-03-31 14:16:04 +0200

    add axios & vue

--- resources/views/templates/master3.blade.php
+++ resources/views/templates/master3.blade.php
@@ -14,6 +14,9 @@
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/vue.js') }}"></script>
+    <script src="{{ asset('js/axios.min.js') }}"></script>
+
 
 </head>
 
