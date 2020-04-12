2d6df1d0fbf7c9a13258c7b4b189c227ec8eda9b
marcussfarre <57440976+marcussfarre@users.noreply.github.com>
2020-04-12 12:42:38 +0200

    Imatges landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,19 +7,19 @@
 @section('principal')
 
 <div class="bodyPrincipal">
-   
+
     <div class="queFem">
         <div class="text">
             <h2>Que Fem?</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
 
-        <img src="img/primerauxilio.jpg" alt="">
+        <img src="img/primerosAuxilios.jpg" alt="" style="width: 250px" >
 
     </div>
 
     <div class="quiSom">
-        <img src="img/clip-bad-gateaway.jpg" alt="">
+        <img src="img/teletrabajo.jpg" alt="" style="margin-right:5px; width: 400px">
         <div class="text">
             <h2>Qui Som?</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
